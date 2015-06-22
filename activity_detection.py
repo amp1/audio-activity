6265aef2e35506a2264dd95e5d42124ad4558fc2
Mikko Pelkonen <amp@ubik.dhcp.inet.fi>
2015-06-22 19:11:27 +0300

    threshold detection [incomplete]

--- activity_detection.py
+++ activity_detection.py
@@ -1,32 +1,52 @@
 import scikits.audiolab as al
 import numpy as np
-import os
-import re
+import os, re
 
 audio_path = "audio_assignment/"
 
-def log_aad():
-    pass
+def aad():
+    files = filelist(audio_path)
+    sounds = []
+    thresholds = []
+    for path in files:
+        sounds.append(read_file(path))
+    for sound in sounds:
+        thresholds.append(threshods(sound))
+        
+def thresholds(x):
+    window_len = 10
+    frame_ms = 10
+    x, frame_size = frames(x)
+    x = log_energy(x, frame_size)
+    smooth = np.r_[x[window_len-1:0:-1],x,x[-1:-window_len:-1]]
+    w = np.hamming(window_len)
+    smooth = np.convolve(w/w.sum(),smooth,mode='valid')
+    W_ms = 1.0e4 # 1*10^4
+    W_len = W_ms/frame_ms*(1000.0/samplerate)
+    t = np.zeros_like(x)
+    for i in range(len(smooth)):
+        e = np.r_[smooth[W_len-1:0:-1], smooth, smooth[-1:-W_len:-1]]
+        t[i] = min(e)+max(2, 0.7*sum(e)/(len(e)-min(e)))
+    return smooth, t
 
-def log_energy(signal):
-    return np.sum(np.log(signal**2))
+def log_energy(signal, frame_size=80):
+    return np.sum(np.log10(signal**2), 1)/frame_size
 
-def energy(signal):
-    a.split(10)
-
-def frames(signal, samplerate=8000, frame_ms=10):
+def frames(sound, samplerate=8000, frame_ms=10):
+    """ Frames as 2-D numpy.array, no windowing """
     samples = 1000.0/samplerate
     frame_size = frame_ms/samples
-    padding = frame_size-(len(a)%frame_size)
-    a = np.append(a, np.zeroes(padding)+0.0001)
-    return np.reshape(a, (-1, frame_size))
+    padding = frame_size-(len(sound)%frame_size)
+    sound = np.append(sound, np.zeros(padding)).clip(min=1e-06)
+    return np.reshape(sound, (-1, frame_size)), frame_size
 
-def filelist():
+def filelist(path):
     audio_files = []
-    for fn in os.listdir(audio_path):
+    for fn in os.listdir(path):
         if re.search("(wav|ogg|flac|mp3)$", fn):
-            audio_files.append(audio_path+fn)
+            audio_files.append(path+fn)
     return audio_files
 
-def read_file(fn):
-    return al.Sndfile(fn, 'r')
+def read_file(path):
+    soundfile = al.Sndfile(path, 'r')
+    return soundfile.read_frames(soundfile.nframes)
--- interact.py
+++ interact.py
@@ -1,4 +1,9 @@
 import activity_detection as ad
 import scikits.audiolab as al
 from matplotlib.pyplot import *
+from scipy import signal
 import numpy as np
+
+sounds = []
+for path in ad.filelist(ad.audio_path):
+    sounds.append(ad.read_file(path))
